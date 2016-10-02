5f6b7f15533b214806c84b63130030fbf68cfe05
Tyler C <tyler.corum@gmail.com>
2016-10-01 22:26:01 -0700

    added Even, Even works perfect, updated index, updated drills with link to Even, still need to bang the kinks out of the header, 1px off.  shoot.

--- drills/even.js
+++ drills/even.js
@@ -1,43 +1,57 @@
 //EVEN 
 //Use: to find out if a number is even or odd, and then asks the user if they want to run the program again.
+//version 1.0.1 - testing.  Stable.
 
-//var keyboardInput = NaN;
-playNow = true;
-evenStart = 0
 
-do {
 
-		do {
-			if ( evenStart == 0 ) {
+$(document).ready(function() {
+
+	//click here to begin popups
+	$("#launch").click(function() {
+	playNow = true;
+	evenStart = 0;
+	
+	//start of actualy Even application
+	do { 				//while you still want to play/run app
+
+		do {			//keep looping until you get a positive integer
+			if ( evenStart == 0 ) {			//only show welcome message on first run
 				keyboardInput = window.prompt('Welcome to Even, please enter a number that\'s larger than zero to find out of it is even or odd, then press OK.');
-				evenStart++;
+				evenStart++;		//immediately increment so it loops through the other 2 options if non-positive int
+				if ( keyboardInput == null) {		//if user presses "Cancel"
+					return;
+				};
+							//if user inputs words or symbols
 			} else if ( isNaN(keyboardInput) ) {keyboardInput = window.prompt('Please enter a number, nothing else:');
+				if ( keyboardInput == null) {
+				return;
+				};
+							//if user inputs 0 or -1/neg int
 			}	else if ( keyboardInput <= 0 ) {keyboardInput = window.prompt('The number must be larger than zero:');
-			};
+				if ( keyboardInput == null) {
+				return;
+				};
+			};			//keep looping until you get a positive integer
 		} while ( isNaN(keyboardInput) || keyboardInput <= 0 );
 
-		var toBeProcessed = keyboardInput;
-		isODD = ( toBeProcessed % 2 );
-		firstRun = false;
+		//math to figure out if there is a remainder.  ex: 10 / 2 = 5.0, no remainder, evenly devided in two.
+		isODD = ( keyboardInput % 2 );
 
-		if ( isODD == 0) {
-			
+		
+		if ( isODD == 0) {		//even modulus result
 			playNow = window.confirm("This number (" + keyboardInput + ") is Even.  Do you need to work with any other numbers?");
+			evenStart = 0;
 			
-		} else if (isODD == 1) {
-			
+		} else if (isODD == 1) {		//odd modulus result
 			playNow = window.confirm("This number (" + keyboardInput + ") is Odd.  Do you need to work with any other numbers?");
 			evenStart = 0;
 			
-		} else {alert("ERROR");
+		} else {		//just in case... never seen it popup yet
+			alert("ERROR");
 		};
-		
+				
 } while (playNow == true);
-
-
-
-
-
-
-
-
+//end click-launch function
+//extra space before close of click function
+	});
+ });
